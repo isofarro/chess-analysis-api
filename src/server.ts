8c8e489c32ea249319c646fd02f08b210f18c888
MikeD <isofarro2@gmail.com>
2025-12-27 20:23:33 +0000

    set standard ports and host

--- src/server.ts
+++ src/server.ts
@@ -3,8 +3,8 @@ import { buildApp } from "./app.js";
 const start = async () => {
   const app = await buildApp();
   try {
-    const port = Number(process.env.PORT) || 3000;
-    await app.listen({ port, host: "0.0.0.0" });
+    const port = Number(process.env.PORT) || 2851;
+    await app.listen({ port, host: "127.0.0.1" });
   } catch (err) {
     app.log.error(err);
     process.exit(1);
